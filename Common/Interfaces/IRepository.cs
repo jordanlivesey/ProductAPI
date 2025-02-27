3242dda5e8c796737dbef20cdf8240f1c1aab461
Jordan.Livesey <Jordan.livesey@triscansystems.com>
2025-02-27 09:10:42 +0000

    Add the Repository interfaces

---
+++ Common/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Interfaces
+{
+    public interface IRepository<Key, Model, Database> where Model : IRepositoryModel<Key>
+    {
+        Task<IEnumerable<Model>> Get();
+        Task<Model> Get(Key id);
+        Task<IEnumerable<Model>> Create(Model model);
+        Task<IEnumerable<Model>> Update(Model model);
+        Task Delete(Model model);
+    }
+}
